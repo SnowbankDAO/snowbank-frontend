e56a17714a31ae13d6c9e0e4ebcc95379daa254c
barenfels <barenfels@gmail.com>
2021-12-06 19:28:56 +0000

    chore: removed debug true for i18n

--- src/i18n.js
+++ src/i18n.js
@@ -24,7 +24,7 @@ i18n.use(initReactI18next) // passes i18n down to react-i18next
         ns: ["common", "bond", "globe", "stake"],
         fallbackLng: "en",
         lng: "en",
-        debug: true,
+        debug: false,
         interpolation: {
             escapeValue: false, // react already safes from xss
         },
