f50182121d308fe00f8a6ac3e34bf0c7703065cf
barenfels <95524514+barenfels@users.noreply.github.com>
2021-12-14 11:43:47 +0100

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,15 +2,10 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en";
-import it from "./locales/it";
 import de from "./locales/de";
 
 const resources = {
     en: en,
-    it: {
-        lang: en.lang,
-        ...it,
-    },
     de: {
         lang: en.lang,
         ...de,
