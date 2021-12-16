d67b421d7227eda352edd208553a94f2a07ab2ab
0xOtoguro <93055914+0xOtoguro@users.noreply.github.com>
2021-12-16 19:18:37 +0100

    feat: add 2 blocks on dashboard, rfv and delta rfv / mkt price (#17)

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -23,6 +23,29 @@ function Dashboard() {
                     <Grid container spacing={4}>
                         <Grid item lg={6} md={6} sm={6} xs={12}>
                             <div className="dashboard-card">
+                                <p className="card-title">{t("RiskFreeValue")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(app.rfv)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("RiskFreeValueDelta")}</p>
+                                <p className="card-value">{isAppLoading ? <Skeleton width="250px" /> : `${trim(Number(app.deltaMarketPriceRfv), 2)}%`}</p>
+                            </div>
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
                                 <p className="card-title">{t("SBPrice")}</p>
                                 <p className="card-value">{isAppLoading ? <Skeleton width="100px" /> : `$${trim(app.marketPrice, 2)}`}</p>
                             </div>
@@ -117,7 +140,7 @@ function Dashboard() {
                             </div>
                         </Grid>
 
-                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                        {/* <Grid item lg={6} md={6} sm={6} xs={12}>
                             <div className="dashboard-card">
                                 <p className="card-title">{t("BackingPerSB")}</p>
                                 <p className="card-value">
@@ -140,7 +163,7 @@ function Dashboard() {
                                 <p className="card-title">{t("Runway")}</p>
                                 <p className="card-value">{isAppLoading ? <Skeleton width="250px" /> : `${trim(Number(app.runway), 1)} Days`}</p>
                             </div>
-                        </Grid>
+                        </Grid> */}
                     </Grid>
                 </Zoom>
             </div>
