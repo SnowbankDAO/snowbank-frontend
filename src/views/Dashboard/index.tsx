69af67ff0aa7db87c61da7c2705d169a933eb50a
cybertelx <cybertelx@protonmail.com>
2021-12-28 10:42:05 +0000

    RFV index

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -38,6 +38,23 @@ function Dashboard() {
                                 </p>
                             </div>
                         </Grid>
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
+                                <p className="card-title">{t("RiskFreeValueWSSB")}</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="250px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 2,
+                                            minimumFractionDigits: 2,
+                                        }).format(app.rfv)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid>
                         {/* <Grid item lg={6} md={6} sm={6} xs={12}>
                             <div className="dashboard-card">
                                 <p className="card-title">{t("RiskFreeValueDelta")}</p>
