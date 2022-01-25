f01fdd190991a693aa4590f1b743df8f63cda84f
barenfels <barenfels@gmail.com>
2022-01-25 14:04:57 +0000

    Chore: merge of Wrap translation file into new locale hyerarchy

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -3,5 +3,7 @@ import bond from "./bond.json";
 import common from "./common.json";
 import globe from "./globe.json";
 import stake from "./stake.json";
+import wrap from "./wrap.json";
+import redeem from "./redeem.json";
 
-export default { lang, bond, common, globe, stake };
+export default { lang, bond, common, globe, stake, wrap, redeem };
